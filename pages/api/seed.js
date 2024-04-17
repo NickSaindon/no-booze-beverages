a6c7ebbc34e302b7ab2046ce1e837d5230c5609d
Nick Saindon <ntimeproductions@gmail.com>
2024-04-16 22:48:20 -0400

    add product data

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,15 +1,11 @@
-import Category from '../../models/Category';
-import User from '../../models/User';
+import Product from '../../models/Product';
 import data from '../../utils/data';
 import db from '../../utils/db';
 
 const handler = async (req, res) => {
   await db.connect();
-  await User.deleteMany();
-  await User.insertMany(data.users);
-  await Category.deleteMany();
-  await Category.insertMany(data.categories);
-
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: 'seeded successfully' });
 };
